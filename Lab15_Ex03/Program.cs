123fa25b22b24b95eca9d50306a4b3b6f32641c0
senashino <phwn26@gmail.com>
2025-03-22 03:31:19 +0700

    update

---
+++ Lab15_Ex03/Program.cs
@@ -0,0 +1,45 @@
+﻿using static System.Console;
+
+var animalA = new Dog();
+var animalB = new Fish();
+var animalC = new Bird();
+
+animalA.Move();
+animalB.Move();
+animalC.Move();
+
+class Animal
+{
+    // Virtual method to be overridden by derived classes
+    public virtual void Move()
+    {
+        WriteLine("Move successfully.");
+    }
+}
+
+class Dog : Animal
+{
+    // Overriding Move method for Dog
+    public override void Move()
+    {
+        WriteLine($"{this.GetType()}: Running on the ground.");
+    }
+}
+
+class Fish : Animal
+{
+    // Overriding Move method for Fish
+    public override void Move()
+    {
+        WriteLine($"{this.GetType()}: Swimming in the water.");
+    }
+}
+
+class Bird : Animal
+{
+    // Overriding Move method for Bird
+    public override void Move()
+    {
+        WriteLine($"{this.GetType()}: Flying in the air.");
+    }
+}
---
+++ Lab15_Ex07/Program.cs
@@ -0,0 +1,33 @@
+﻿using static System.Console;
+
+Derived_2 d2 = new Derived_2();
+Base b = (Base) d2;
+Derived_1 d1 = (Derived_1) d2;
+
+b.A();
+d1.A();
+d2.A();
+
+class Base
+{
+    public virtual void A()
+    {
+        WriteLine("Base.A()");
+    }
+}
+
+class Derived_1 : Base
+{
+    public override void A()
+    {
+        WriteLine("Derived_1.A()");
+    }
+}
+
+class Derived_2 : Derived_1
+{
+    public override void A()
+    {
+        WriteLine("Derived_2.A()");
+    }
+}
